19967e76120fe7e7ee7b0acfa55b130543f86427
nk7260ynpa <nk7260ynpa@gmail.com>
2024-10-31 01:49:57 +0800

    Refactor file structure and add setup.py

--- main.py
+++
@@ -1,4 +0,0 @@
-import twse_crawler
-
-df = twse_crawler.crawler("2024-10-23")
-df.to_csv("2024-10-23.csv", index=False)
---
+++ tw_crawler/__init__.py
@@ -0,0 +1,3 @@
+from .twse import twse_crawler
+from .tpex import tpex_crawler
+from .taifex import taifex_crawler
