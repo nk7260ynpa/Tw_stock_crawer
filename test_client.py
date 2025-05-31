4696371e5e2b30b60fe20f166a3839d3fad67c9c
nk7260ynpa <nk7260ynpa@gmail.com>
2025-05-31 10:20:05 +0800

    Refactor test client to use command-line arguments for data source name and date; Adjust the reveive data process

--- test_client.py
+++ test_client.py
@@ -1,13 +1,26 @@
 import requests
 import pandas as pd
 
-name = "twse"
-date = "2025-05-28"
+import argparse
 
-url = "http://127.0.0.1:6738"
-payload = {"name": name, "date": date}
+def main(opt):
+    NAME = opt.name
+    DATE = opt.date
 
-response = requests.post(url, params=payload)
-df = pd.DataFrame(response.json()["data"])
+    url = "http://127.0.0.1:6738"
+    payload = {"name": NAME, "date": DATE}
 
-print(df.head(2))
+    response = requests.post(url, params=payload)
+    json_data = response.json()["data"]
+    df = pd.read_json(json_data, orient="records")
+
+    print(df.head(2))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Test client for fetching data.")
+    parser.add_argument("--name", type=str, default="twse", help="Name of the data source")
+    parser.add_argument("--date", type=str, default="2025-05-28", help="Date for the data")
+    
+    opt = parser.parse_args()
+    main(opt)
+
