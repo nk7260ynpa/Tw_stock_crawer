7ade91eb6ae53ac1e9281ce72d34399fcd7ddd74
nk7260ynpa <nk7260ynpa@gmail.com>
2025-05-31 10:19:07 +0800

    Fix data serialization in craw_data function and add newline in server.sh

--- server.py
+++ server.py
@@ -9,4 +9,4 @@ def craw_data(name, date):
     crawlertype = name+"_crawler"
     crawler = getattr(tw_crawler, crawlertype)  # Dynamically get the class or method
     df = crawler(date)
-    return {"data": df.to_dict(orient="records")}
+    return {"data": df.to_json(orient="records")}
--- server.sh
+++ server.sh
@@ -1,1 +1,1 @@
-uvicorn server:app --host 0.0.0.0 --port 6738
+uvicorn server:app --host 0.0.0.0 --port 6738 
