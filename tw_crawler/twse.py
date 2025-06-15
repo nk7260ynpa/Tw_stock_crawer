a8eda67217a368e63a60ca5f590b796a31d301a1
Chen <nk7260ynpa@gmail.com>
2025-06-15 08:10:30 +0800

    Fix LastBestBidVolume and LastBestAskVolume string replacement logic in post_process function

--- tw_crawler/twse.py
+++ tw_crawler/twse.py
@@ -106,9 +106,9 @@ def post_process(df, date) -> pd.DataFrame:
     df["Change"] = df["Change"].str.replace(",", "").str.replace("--", "0").astype(float)
     df["Change"] = df["Change"] * df["Dir"]
     df["LastBestBidPrice"] = df["LastBestBidPrice"].str.replace(",", "").str.replace("--", "0").astype(float)
-    df["LastBestBidVolume"] = df["LastBestBidVolume"].str.replace(",", "").str.replace("--", "0").astype(int)
+    df["LastBestBidVolume"] = df["LastBestBidVolume"].str.replace("", "0").str.replace(",", "").str.replace("--", "0").astype(int)
     df["LastBestAskPrice"] = df["LastBestAskPrice"].str.replace(",", "").str.replace("--", "0").astype(float)
-    df["LastBestAskVolume"] = df["LastBestAskVolume"].str.replace(",", "").str.replace("--", "0").astype(int)
+    df["LastBestAskVolume"] = df["LastBestAskVolume"].str.replace("", "0").str.replace(",", "").str.replace("--", "0").astype(int)
     df["PriceEarningratio"] = df["PriceEarningratio"].str.replace(",", "").astype(float)
 
     df = df.drop(columns=["Dir"])
