31f87fc0321dcc4f18bc938c75b8159959c5aea9
Chen <nk7260ynpa@gmail.com>
2025-06-20 00:19:54 +0800

    Add logging to the faoi_crawler function for better traceability

--- tw_crawler/faoi.py
+++ tw_crawler/faoi.py
@@ -1,6 +1,16 @@
 import requests
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def fetch_faoi_data(date):
     url = f'https://www.twse.com.tw/rwd/zh/fund/T86?date={date.replace("-", "")}&selectType=ALL&response=json'
     response = requests.get(url)
     return response.json()
+
+def faoi_crawler(date):
+    logger.info(f"Starting Request data from Foreign and Other Investors")
+    response = fetch_faoi_data(date)
+    df = parse_faoi_data(response, date)
+    return df
