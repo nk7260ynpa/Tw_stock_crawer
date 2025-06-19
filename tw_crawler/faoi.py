743fd8975e2b39da57c0a51ac0c38c057b0bb46d
Chen <nk7260ynpa@gmail.com>
2025-06-19 20:22:13 +0800

    Add fetch_faoi_data function to retrieve FAOI data from TWSE

---
+++ tw_crawler/faoi.py
@@ -0,0 +1,6 @@
+import requests
+
+def fetch_faoi_data(date):
+    url = f'https://www.twse.com.tw/rwd/zh/fund/T86?date={date.replace("-", "")}&selectType=ALL&response=json'
+    response = requests.get(url)
+    return response.json()
