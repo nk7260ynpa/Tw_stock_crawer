4e7df195e84d536a1edb5d163c5c6e6592994bd3
Chen <nk7260ynpa@gmail.com>
2025-06-20 00:21:45 +0800

    Refactor parse_faoi_data function for improved data handling

--- tw_crawler/faoi.py
+++ tw_crawler/faoi.py
@@ -1,8 +1,18 @@
 import requests
 import logging
 
+import pandas as pd
+
 logger = logging.getLogger(__name__)
 
+
+def parse_faoi_data(response, date):
+    if response["stat"] == "OK":
+        df = pd.DataFrame(columns=response["fields"], data=response["data"])
+        df = post_process(df, date)
+    else:
+        df = gen_empty_date_df()
+    return df
 
 def fetch_faoi_data(date):
     url = f'https://www.twse.com.tw/rwd/zh/fund/T86?date={date.replace("-", "")}&selectType=ALL&response=json'
