7c13fee5b859356189ddefc6645e51c72796758a
Chen <nk7260ynpa@gmail.com>
2025-06-29 20:24:34 +0800

    Refactor parse_mgts_data function to improve JSON response parsing and DataFrame creation

--- tw_crawler/mgts.py
+++ tw_crawler/mgts.py
@@ -1,7 +1,29 @@
 import requests
 import logging
 
+import pandas as pd
+
 logger = logging.getLogger(__name__)
+
+def parse_mgts_data(response, date):
+    """
+    Parse the JSON response from the MGTS website into a DataFrame.
+
+    Args:
+        data (dict): The JSON response from the MGTS website.
+
+    Returns:
+        pd.DataFrame: The parsed DataFrame.
+
+    Examples:
+        >>> parse_mgts_data(data)
+    """
+    if response["stat"] == "OK":
+        df = pd.DataFrame(columns=response["tables"][1]["fields"], data=response["tables"][1]["data"])
+        df = post_process(df, date)
+    else:
+        df = gen_empty_date_df()
+    return df
 
 def fetch_mgts_data(date):
     """
