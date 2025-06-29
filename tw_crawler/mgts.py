3f5f522ae0ac27e94e429687246b8172ec5ba0ee
Chen <nk7260ynpa@gmail.com>
2025-06-29 20:08:27 +0800

    Add logging to mgts.py for improved debugging and tracking

--- tw_crawler/mgts.py
+++ tw_crawler/mgts.py
@@ -1,4 +1,7 @@
 import requests
+import logging
+
+logger = logging.getLogger(__name__)
 
 def fetch_mgts_data(date):
     """
@@ -16,3 +19,21 @@ def fetch_mgts_data(date):
     url = f'https://www.twse.com.tw/rwd/zh/marginTrading/MI_MARGN?date={date.replace("-", "")}&selectType=ALL&response=json'
     response = requests.get(url)
     return response.json()
+
+def mgts_crawler(date):
+    """
+    Crawl the MGTS website for stock data on a given date and process it.
+
+    Args:
+        date (str): The date in 'YYYY-MM-DD' format.
+
+    Returns:
+        pd.DataFrame: The processed DataFrame containing stock data.
+
+    Examples:
+        >>> mgts_crawler("2022-02-18")
+    """
+    logger.info(f"Starting Request data from Foreign and Other Investors")
+    response = fetch_mgts_data(date)
+    df = parse_mgts_data(response, date)
+    return df
